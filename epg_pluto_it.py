fbea2570ce6beb6120de1fcb650d087e51bc474b
kiekostui <semar74@libero.it>
2025-10-11 23:35:27 +0200

    Update epg_pluto_it.py

--- epg_pluto_it.py
+++ epg_pluto_it.py
@@ -262,7 +262,7 @@ def get_appversion():
     url = "https://pluto.tv/"
     headers={
         'upgrade-insecure-requests': '1',
-        'user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36)
+        'user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36')
         }
 
     try:    
